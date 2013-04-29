a7d776798c607737a27962938d70956d9b242fd7
Jeffrey M Lutz <jeff.lutz@agileanswers.com>
2013-04-29 16:29:54 -0400

    assignment 1 compute cost done

--- machine_learning_coursera/assignment_1_linear_regression/ex1/computeCost.m
+++ machine_learning_coursera/assignment_1_linear_regression/ex1/computeCost.m
@@ -13,9 +13,11 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+sum = 0;
+for i=1:m
+  hyp = theta(1) + theta(2)*X(i);
+  J = J + (hyp - y(i))^2 / (2 * m);
+end
 
 % =========================================================================
 
